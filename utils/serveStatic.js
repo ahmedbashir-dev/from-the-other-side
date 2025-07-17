8528ffd261befa7aae7313d1cc3a5c226482f64b
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-07-18 03:25:01 +0500

    Serve the frontend - refactored serveStatic and sendResponse

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -3,14 +3,14 @@ import fs from "node:fs/promises"
 import { sendResponse } from "./sendResponse.js";
 import { getContentType } from "./getContentType.js";
 
-export async function serveStatic(req, res, baseDir, status) {
+export async function serveStatic(req, res, baseDir) {
     const publicDir = path.join(baseDir, 'public');
     const pathToResource = path.join(publicDir, req.url === '/' ? 'index.html' : req.url);
     const ext = path.extname(pathToResource);
     const contentType = getContentType(ext);
     try {
         const content = await fs.readFile(pathToResource);
-        sendResponse(res, status, contentType, content);
+        sendResponse(res, 200, contentType, content);
     } catch (err) {
         console.log(err);
     }
